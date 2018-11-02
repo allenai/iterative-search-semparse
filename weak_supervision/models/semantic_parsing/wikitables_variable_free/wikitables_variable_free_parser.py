e5a772b25af7955d09af8ff97e6b0143d6b099f7
Pradeep Dasigi <pradeep.dasigi@gmail.com>
2018-11-02 13:53:21 -0700

    script for decoding from an erm model (#13)

--- weak_supervision/models/semantic_parsing/wikitables_variable_free/wikitables_variable_free_parser.py
+++ weak_supervision/models/semantic_parsing/wikitables_variable_free/wikitables_variable_free_parser.py
@@ -637,29 +637,36 @@ class WikiTablesVariableFreeParser(Model):
         outputs['best_action_sequence'] = []
         outputs['debug_info'] = []
         outputs['entities'] = []
-        outputs['logical_form'] = []
+        outputs['logical_form']: List[List[str]] = []
         for i in range(batch_size):
             # Decoding may not have terminated with any completed logical forms, if `num_steps`
             # isn't long enough (or if the model is not trained enough and gets into an
             # infinite action loop).
+            outputs['logical_form'].append([])
             if i in best_final_states:
-                best_action_indices = best_final_states[i][0].action_history[0]
-                action_strings = [action_mapping[(i, action_index)] for action_index in best_action_indices]
-                try:
-                    logical_form = world[i].get_logical_form(action_strings, add_var_function=False)
-                    self._has_logical_form(1.0)
-                except ParsingError:
-                    self._has_logical_form(0.0)
-                    logical_form = 'Error producing logical form'
-                if target_list:
-                    denotation_correct = world[i].evaluate_logical_form(logical_form, target_list[i])
-                    self._denotation_accuracy(1.0 if denotation_correct else 0.0)
-                outputs['best_action_sequence'].append(action_strings)
-                outputs['logical_form'].append(logical_form)
+                all_action_indices = [best_final_states[i][j].action_history[0] for j in
+                                      range(len(best_final_states[i]))]
+                for j, action_indices in enumerate(all_action_indices):
+                    action_strings = [action_mapping[(i, action_index)] for action_index in action_indices]
+                    has_logical_form = False
+                    try:
+                        logical_form = world[i].get_logical_form(action_strings, add_var_function=False)
+                        has_logical_form = True
+                    except ParsingError:
+                        logical_form = 'Error producing logical form'
+                    if j == 0:
+                        if has_logical_form:
+                            self._has_logical_form(1.0)
+                        else:
+                            self._has_logical_form(0.0)
+                        if target_list:
+                            denotation_correct = world[i].evaluate_logical_form(logical_form, target_list[i])
+                            self._denotation_accuracy(1.0 if denotation_correct else 0.0)
+                        outputs['best_action_sequence'].append(action_strings)
+                    outputs['logical_form'][-1].append(logical_form)
                 outputs['debug_info'].append(best_final_states[i][0].debug_info[0])  # type: ignore
                 outputs['entities'].append(world[i].table_graph.entities)
             else:
-                outputs['logical_form'].append('')
                 self._has_logical_form(0.0)
                 self._denotation_accuracy(0.0)
         if metadata is not None:
